b9824d51f2e7c510bbd52ae9530d97c8bec5ab65
KazimierzMigdal <kazmierz.migdal@protonmail.com>
2020-04-27 14:12:46 +0200

    Password Reset email adress added

--- Bombelka/ZaUsmiechBombelka/settings.py
+++ Bombelka/ZaUsmiechBombelka/settings.py
@@ -150,5 +150,5 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-EMAIL_HOST_USER = 'migdal.kazimierz@gmail.com'
-EMAIL_HOST_PASSWORD = 'qzddpajppzjfjiii'
+EMAIL_HOST_USER = 'zausmiechbombelka@gmail.com'
+EMAIL_HOST_PASSWORD = 'bumbguxpsucypyzs'
